2aa57b16b3a4b611df3023d79cc3ed413a08baf3
joshpaq <joshua.m.paquette@gmail.com>
2020-01-28 21:56:24 -0600

    wip: working on layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import Title from './Title'
+
+const Container = styled.div`
+  font-family: 'Josefin Sans';
+  text-transform: uppercase;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 1em;
+  padding-right: 1em;
+  max-width: 768px;
+`
+
+const Section = styled.div`
+  padding: 1em;
+`
+
+const Header = styled.div`
+  font-size: 2em;
+`
+
+const Content = styled.div`
+  padding: 1em;
+`
 
 const App = () => (
-  <div>
-    Josh + Kate
-  </div>
+  <Container>
+    <Title />
+    <Section>
+      <Header>
+        When
+      </Header>
+      <Content>
+        Saturday, June 13, 2020
+      </Content>
+    </Section>
+    <Section>
+      <Header>
+        Where
+      </Header>
+      <Content>
+        Exploration Place, 300 McClean Boulevard, Wichita, KS
+      </Content>
+    </Section>
+  </Container>
 )
 
 export default App
---
+++ src/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Content = styled.div`
+  text-align: center;
+  padding: .5em;
+  font-size: 3em;
+`
+
+const Title = () => (
+  <Content>Josh + Kate</Content>
+)
+
+export default Title
